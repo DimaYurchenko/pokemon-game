8cf0b0a7a3e0082a212cbf0f732ee481edf0b0be
Dmitriy Yurchenko <udd2006@gmail.com>
2021-03-12 11:47:48 +0100

    Update index.js

--- src/components/Layout Block/index.js
+++ src/components/Layout Block/index.js
@@ -2,14 +2,14 @@ import s from './style.module.css';
 
 const Layout = ({title, desc, urlBg, colorBg}) => {
     return (
-        <section className={s.root} backgroundImage = {urlBg}>
+        <section className={s.root} style={{background: urlBg ? `url(${urlBg})` : colorBg}}>
             <div className={s.wrapper} >
                 <article>
                     <div className={s.title}>
                         <h3>{title}</h3>
                         <span className={s.separator}></span>
                     </div>
-                    <div className={s.desc, s.full}> 
+                    <div className={`${s.desc} ${s.full}`}> 
                         <p>{desc}</p>
                     </div>
                 </article>
